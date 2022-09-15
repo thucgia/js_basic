c9b50ea9901ea4f09c81744cd0a668539d6b4f9b
ntthuc <ntthuc@tma.com.vn>
2022-09-15 10:51:34 +0700

    step2

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
-var todolist = []
-
-const checkValidate = () => {
-	let name_field = document.forms["todo"]["name"].value
-	if (name_field == "") {
-		alert("Name must be filled out")
-		return false
-	}
-	return true
-}
+var todolistMul = []
+var sort_type = {type : "", count : 0}
 
 const checkValidateMulti = () => {
 	let name_field = document.forms["todo-multi"]["name"].value
 	let color_field = document.forms["todo-multi"]["color"].value
-	if (name_field == "" || color_field == "") {
-		alert("Name & Color must be filled out")
+	if (name_field == "") {
+		alert("Name must be filled out")
+		return false
+	}
+	if (color_field == "") {
+		alert("Color must be filled out")
 		return false
 	}
 	return true
@@ -22,165 +18,79 @@ const checkValidateMulti = () => {
 const checkValidateMultiUpd = () => {
 	let name_field = document.forms["update-element"]["name"].value
 	let color_field = document.forms["update-element"]["color"].value
-	if (name_field == "" || color_field == "") {
-		alert("Name & Color must be filled out")
+	if (name_field == "") {
+		alert("Name must be filled out")
+		return false
+	}
+	if (color_field == "") {
+		alert("Color must be filled out")
 		return false
 	}
 	return true
 }
 
-var listShow = () => {
-	var show_data_test = todolist
-	const ul = document.getElementById("myUL")
-	ul.innerHTML = ''
-
-	var items_close = ul.getElementsByClassName('close')
-	var items_edit = ul.getElementsByClassName('edit')
-
-	show_data_test.map(item => {
-		ul.append(createLiComponent(item))
-		document.getElementById("myInput").value = "";
-
-		for (let i = 0; i < items_close.length; i++) {
-			items_close[i].onclick = function() {
-				todolist.splice(i, 1)
-				listShow()
-			}
-		}
-	
-		for (let i = 0; i < items_edit.length; i++) {
-			items_edit[i].onclick = function() {
-				var listItem=this.parentElement;
-	
-				var editInput=listItem.querySelector('input[type=text]');
-				var label=listItem.querySelector("label");
-				var containsClass=listItem.classList.contains("editMode");
-					//If class of the parent is .editmode
-					if(containsClass){
-						todolist[i] = editInput.value
-						listShow()
-					}else{
-						editInput.value=label.innerText;
-					}
-	
-					//toggle .editmode on the parent.
-					listItem.classList.toggle("editMode");
-			}
-		}
-	})
-}
-
-var newElement = () => {
-	if (!checkValidate()) return
-	const value = document.getElementById("myInput").value
-	todolist.push(value)
-	listShow()
-}
-
-// --------------------- object -------------
-var todolistMul = []
-var newElementMulti = () => {
+var createItem = () => {
 	if (!checkValidateMulti()) return
 	const value = {}
 	value.name = document.getElementById("myInputName").value
 	value.color = document.getElementById("myInputColor").value
 	todolistMul.push(value)
-	listShowMulti()
+	showItems(todolistMul)
+	changePage(1)
 }
 
-var listShowMulti = () => {
-	var show_data_multi = todolistMul
-	const ul = document.getElementById("myUL-multi")
-	ul.innerHTML = ''
+var showItems = (arr) => {
+	var todo_results = [...arr]
+	const tbody = document.getElementById("my-table").getElementsByTagName("tbody")[0]
+	tbody.innerHTML = ''
 	
-	var items_close = ul.getElementsByClassName('close')
-	var items_edit = ul.getElementsByClassName('edit')
-	show_data_multi.map(item => {
-		ul.append(createLiComponent(item.name, true, item.color))
+	var items_close = document.getElementsByClassName('close')
+	var items_edit = document.getElementsByClassName('edit')
+	todo_results.map(item => {
+		createLiComponent(tbody, item.name, item.color)
 		document.getElementById("myInputName").value = "";
 		document.getElementById("myInputColor").value = "";
 
 		for (let i = 0; i < items_close.length; i++) {
 			items_close[i].onclick = function() {
-				todolistMul.splice(i, 1)
-				listShowMulti()
+				todolistMul.splice((current_page - 1) * records_per_page + i, 1)
+				showItems(todolistMul)
+				current_page = 1
+				changePage(1)
 			}
 		}
 
 		for (let i = 0; i < items_edit.length; i++) {
 			items_edit[i].onclick = function() {
-				var listItem=this.parentElement;
-	
-				// var editInput=listItem.getElementsByTagName('input');
-				var label=listItem.getElementsByTagName("label");
-				// var containsClass=listItem.classList.contains("editMode-multi");
-				// //If class of the parent is .editmode
-				// if(containsClass){
-				// 	todolistMul[i]["name"] = editInput[0].value
-				// 	todolistMul[i]["color"] = editInput[1].value
-				// 	listShowMulti()
-				// }else{
-				// 	editInput[0].value=label[0].innerText;
-				// 	editInput[1].value=label[1].innerText;
-				// }
-
-				// //toggle .editmode on the parent.
-				// listItem.classList.toggle("editMode-multi");
 				modal.style.display = "block";
 				document.getElementById("id-upd").value = i
-				document.forms["update-element"]["name"].value = label[0].innerText; 
-				document.forms["update-element"]["color"].value = label[1].innerText;
+				document.forms["update-element"]["name"].value = todo_results[i].name
+				document.forms["update-element"]["color"].value = todo_results[i].color
 			}
 		}
 	})
 }
 
-var createLiComponent = (value, multi = false, value_2 = "") => {
-	let li = document.createElement("li")
+var createLiComponent = (tbody, value1, value2) => {
+	var new_row = tbody.insertRow()
+
+	var new_cell_name = new_row.insertCell()
+	var new_text_name = document.createTextNode(value1)
+	new_cell_name.append(new_text_name)
+	
+	var new_cell_color = new_row.insertCell()
+	var new_text_color = document.createTextNode(value2)
+	new_cell_color.append(new_text_color)
+
+	var new_cell_action = new_row.insertCell()
 	var span_delete = document.createElement("SPAN");
 	var txt_delete = document.createTextNode("\u00D7");
-
+	span_delete.className = "close";
+	span_delete.appendChild(txt_delete);new_cell_action.append(span_delete)
 	var span_edit = document.createElement("SPAN");
 	var txt_edit = document.createTextNode("\u270E");
-
-	
-
-	if (multi) {
-		var label = document.createElement("LABEL")
-		var input_edit = document.createElement("INPUT")
-		input_edit.type = "text"
-		input_edit.className="multi"
-
-		var label_2 = document.createElement("LABEL")
-		var input_edit_2 = document.createElement("INPUT")
-		input_edit_2.type = "text"
-		input_edit_2.className="multi"
-	} else {
-		var label = document.createElement("LABEL")
-		var input_edit = document.createElement("INPUT")
-		input_edit.type = "text"
-	}
-
-	span_delete.className = "close";
-	span_delete.appendChild(txt_delete);
-
 	span_edit.className = "edit";
-	span_edit.appendChild(txt_edit);
-	
-	label.innerText = value
-	li.appendChild(label);
-	li.appendChild(input_edit);
-	if (multi) {
-		var span_div = document.createElement("B")
-		span_div.innerText = " - "
-		label_2.innerText = value_2
-		li.appendChild(span_div);
-		li.appendChild(label_2);
-		li.appendChild(input_edit_2);
-	}
-	li.appendChild(span_delete);
-	li.appendChild(span_edit);
-	return li
+	span_edit.appendChild(txt_edit);new_cell_action.append(span_edit)
 }
 
 var sort_data = (obj) => {
@@ -191,28 +101,89 @@ var sort_data = (obj) => {
 		datasortName.sort((a, b) => {
 			const nameA = a.name.toUpperCase()
 			const nameB = b.name.toUpperCase()
-			if (nameA < nameB) return -1
-			if (nameA > nameB) return 1
+			if (nameA < nameB) return sort_type["type"] == type && (sort_type["count"] % 2 != 0) ? 1 : -1
+			if (nameA > nameB) return sort_type["type"] == type && (sort_type["count"] % 2 != 0) ? -1 : 1
 			return 0
 		})
 	} else if (type == "sort-color") {
 		datasortName.sort((a, b) => {
 			const nameA = a.color.toUpperCase()
 			const nameB = b.color.toUpperCase()
-			if (nameA < nameB) return -1
-			if (nameA > nameB) return 1
+			if (nameA < nameB) return sort_type["type"] == type && (sort_type["count"] % 2 != 0) ? 1 : -1
+			if (nameA > nameB) return sort_type["type"] == type && (sort_type["count"] % 2 != 0) ? -1 : 1
 			return 0
 		})
 	}
 
-	listShowMulti()
+	sort_type["type"] = type
+	sort_type["count"] ++
+
+	showItems(todolistMul)
+	changePage(current_page)
 }
 
 const updateElement = () => {
 	if (!checkValidateMultiUpd()) return 
 	let index = (document.getElementById("id-upd").value)
-	todolistMul[index]["name"] = document.forms["update-element"]["name"].value
-	todolistMul[index]["color"] = document.forms["update-element"]["color"].value
-	listShowMulti()
+	todolistMul[(current_page - 1) * records_per_page + parseInt(index)]["name"] = document.forms["update-element"]["name"].value
+	todolistMul[(current_page - 1) * records_per_page + parseInt(index)]["color"] = document.forms["update-element"]["color"].value
+	showItems(todolistMul)
+	changePage(current_page)
 	modal.style.display = "none";
+}
+
+var current_page = 1;
+var records_per_page = 3;
+function prevPage()
+{
+    if (current_page > 1) {
+        current_page--;
+        changePage(current_page);
+    }
+}
+
+function nextPage()
+{
+    if (current_page < numPages()) {
+        current_page++;
+        changePage(current_page);
+    }
+}
+
+function changePage(page)
+{
+    var btn_next = document.getElementById("btn_next");
+    var btn_prev = document.getElementById("btn_prev");
+    var page_span = document.getElementById("page");
+
+    // Validate page
+    if (page < 1) page = 1;
+    if (page > numPages()) page = numPages();
+
+    const tbody = document.getElementById("my-table").getElementsByTagName("tbody")[0]
+	tbody.innerHTML = ''
+
+    // for (var i = (page-1) * records_per_page; i < (page * records_per_page) && i < todolistMul.length; i++) {
+    //     createLiComponent(tbody, todolistMul[i].name, todolistMul[i].color)
+    // }
+
+	showItems(todolistMul.slice((page-1) * records_per_page, (page * records_per_page)))
+    page_span.innerHTML = page;
+
+    if (page == 1) {
+        btn_prev.style.visibility = "hidden";
+    } else {
+        btn_prev.style.visibility = "visible";
+    }
+
+    if (page == numPages()) {
+        btn_next.style.visibility = "hidden";
+    } else {
+        btn_next.style.visibility = "visible";
+    }
+}
+
+function numPages()
+{
+    return Math.ceil(todolistMul.length / records_per_page);
 }
